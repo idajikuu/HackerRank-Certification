a182a053d7a1985fc9d0473656acf2d5fdbb2fef
Fatima Zahra Zeghli <120262100+idajikuu@users.noreply.github.com>
2023-05-12 15:02:19 +0100

    Create VendingMachine.py

---
+++ Python (Basic) Skills Certification Test/VendingMachine.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+class VendingMachine:
+    def __init__(self,num_items,item_price):
+        self.num_items = num_items;
+        self.item_price = item_price;
+    def buy(self,req_items,money):
+        if(num_items >= req_items and money >= req_items*self.item_price):
+            self.num_items -= req_items;
+            return money - (req_items*self.item_price);
+        else:
+            if( money < req_items*self.item_price):
+                return f"Insert {self.item_price-money}$"
+            else:
+                return "Not enough items"
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    num_items, item_coins = map(int, input().split())
+    machine = VendingMachine(num_items, item_coins)
+
+    n = int(input())
+    for _ in range(n):
+        num_items, num_coins = map(int, input().split())
+        try:
+            change = machine.buy(num_items, num_coins)
+            fptr.write(str(change) + "\n")
+        except ValueError as e:
+            fptr.write(str(e) + "\n")
+
+
+    fptr.close()
+
